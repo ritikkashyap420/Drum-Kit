d5f573fa3e2ac60ab47ce13e2e2c205220b4a3dc
ritikkashyap420 <57295935+ritikkashyap420@users.noreply.github.com>
2020-10-05 16:25:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+var no_of_button = document.querySelectorAll(".drum").length;
+
+// console.log(no_of_button);
+for (var i = 0; i < no_of_button; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+//SOUND BY MOUSE
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    }
+  );
+}
+
+document.addEventListener("keydown",function(event){
+  makeSound(event.key);
+  console.log(event);
+  console.log(event.key);
+
+});
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var tom_1 = new Audio("sounds/tom-1.mp3");
+      tom_1.play();
+      break;
+
+    case "a":
+      var tom_2 = new Audio("sounds/tom-2.mp3");
+      tom_2.play();
+      break;
+
+    case "s":
+      var tom_3 = new Audio("sounds/tom-3.mp3");
+      tom_3.play();
+      break;
+
+    case "d":
+      var tom_4 = new Audio("sounds/tom-4.mp3");
+      tom_4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default: console.log(key);
+}
+}
